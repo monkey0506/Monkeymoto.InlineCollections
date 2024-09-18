ce6bf97bac7b2e8a4edfec4b5b4722079a995ce6
Michael Rittenhouse <monkey0506@gmail.com>
2024-09-18 05:58:05 -0500

    Make diagnostic descriptors readonly

--- Diagnostics.cs
+++ Diagnostics.cs
@@ -4,7 +4,7 @@ namespace Monkeymoto.InlineCollections
 {
     internal static class Diagnostics
     {
-        public static DiagnosticDescriptor MMIC1000_CollectionBuilderTypeInaccessible_Descriptor = new
+        public static readonly DiagnosticDescriptor MMIC1000_CollectionBuilderTypeInaccessible_Descriptor = new
         (
             "MMIC1000",
             "MMIC1000: Inline collection inaccessible to collection builder",
@@ -14,7 +14,7 @@ namespace Monkeymoto.InlineCollections
             isEnabledByDefault: true
         );
 
-        public static DiagnosticDescriptor MMIC1001_MustHaveValidLength_Descriptor = new
+        public static readonly DiagnosticDescriptor MMIC1001_MustHaveValidLength_Descriptor = new
         (
             "MMIC1001",
             "MMIC1001: Inline collection must have positive, non-zero length",
@@ -24,7 +24,7 @@ namespace Monkeymoto.InlineCollections
             isEnabledByDefault: true
         );
 
-        public static DiagnosticDescriptor MMIC1002_MustBePartialType_Descriptor = new
+        public static readonly DiagnosticDescriptor MMIC1002_MustBePartialType_Descriptor = new
         (
             "MMIC1002",
             "MMIC1002: Inline collection struct and all containing types must be partial",
@@ -34,7 +34,7 @@ namespace Monkeymoto.InlineCollections
             isEnabledByDefault: true
         );
 
-        public static DiagnosticDescriptor MMIC1003_MustDefineExactlyOneField_Descriptor = new
+        public static readonly DiagnosticDescriptor MMIC1003_MustDefineExactlyOneField_Descriptor = new
         (
             "MMIC1003",
             "MMIC1003: Inline collection struct must declare one and only one instance field",
@@ -44,7 +44,7 @@ namespace Monkeymoto.InlineCollections
             isEnabledByDefault: true
         );
 
-        public static DiagnosticDescriptor MMIC1004_CollectionExpressionTooLarge_Descriptor = new
+        public static readonly DiagnosticDescriptor MMIC1004_CollectionExpressionTooLarge_Descriptor = new
         (
             "MMIC1004",
             "MMIC1004: Collection expression is larger than target inline collection",
@@ -54,7 +54,7 @@ namespace Monkeymoto.InlineCollections
             isEnabledByDefault: true
         );
 
-        public static DiagnosticDescriptor MMIC1005_CollectionExpressionMaybeTooLarge_Descriptor = new
+        public static readonly DiagnosticDescriptor MMIC1005_CollectionExpressionMaybeTooLarge_Descriptor = new
         (
             "MMIC1005",
             "MMIC1005: Collection expression may be larger than target inline collection",
